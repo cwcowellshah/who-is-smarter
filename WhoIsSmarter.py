47d25985a75eacec88a1ce87b257a0098742e1c0
Chris Cowell <christopher.w.cowell@gmail.com>
2018-11-20 08:21:01 -0800

    WIP

--- WhoIsSmarter.py
+++ WhoIsSmarter.py
@@ -1,26 +1,30 @@
-# Figure out how many letters (on average) there are per word
+# Calculate average letters per word
 
-# read all the text from a file
-fake_speech_file = open('fake_speech_file.txt')
+# read the text from a file
+# fake_speech_file = open('fake_speech.txt')
+fake_speech_file = open('brown_1.txt')
 fake_speech_text = fake_speech_file.read()
 fake_speech_file.close()
 
 # replace carriage returns with spaces
-brown_1_text = brown_1_text.replace('\n', ' ')
+fake_speech_text = fake_speech_text.replace('\n', ' ')
 
 # replace punctuation with empty strings
+punctuation_marks = ['.', ',', ':', ';', '(', ')', '!', '?', '[', ']', '$', '"', "'", '\t']
+for punctuation_mark in punctuation_marks:
+    fake_speech_text = fake_speech_text.replace(punctuation_mark, '')
 
-
-# chop the text into words, by cutting it every time there's a space
-words_in_speech = brown_1_text.split()
+# chop the text into words, using spaces as word dividers
+words_in_fake_speech = fake_speech_text.split()
+print(words_in_fake_speech)
 
 # count how many characters are in the words
-num_chars_in_speech = 0
-for word in words_in_speech:
-    num_chars_in_speech += len(word)
+num_chars_in_fake_speech = 0
+for word in words_in_fake_speech:
+    num_chars_in_fake_speech += len(word)
 
-# find the mean of the number of characters in words
-num_words_in_speech = len(words_in_speech)
-mean_letters_per_word = num_chars_in_speech / num_words_in_speech
+# find the mean letters per word
+num_words_in_fake_speech = len(words_in_fake_speech)
+mean_letters_per_word = num_chars_in_fake_speech / num_words_in_fake_speech
 
 print(mean_letters_per_word)
